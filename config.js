27fefb7b54602ec99d22ced6782cc2d52163c0da
knebao <116298744+knebao@users.noreply.github.com>
2026-02-02 16:51:00 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+﻿const CONFIG = {
+    introTitle: '3/2 rồi nè!!!',
+    introDesc: `Happy Birthday Bbi`,
+    btnIntro: 'Bấm qua để nhận quà nhóe!',
+    title: 'Chúc em yêu tuổi mới ngày càng xinh đẹp hơn, vui vẻ hơn và hạnh phúc hơn bên anh',
+    desc: 'Trả lời câu hỏi "Anh có đẹp trai hơm?" thử xem anh có nên tặng quà cho em hong nhé',
+    btnYes: 'Anh là đẹp trai nhấtt',
+    btnNo: 'Không!!!',
+    question: 'Câu trả lời này oke á, anh quyết định sẽ tặng em một món quà, nó nằm ở đâu đó trong phòng anh, lục tìm thử xem',
+    btnReply: 'Gửi cho anh',
+    reply: 'Cảm ơn chồng yêu, em yêu anh nhất',
+    mess: 'Anh biết mà. Anh cũng yêu em',
+    messDesc: 'Lại ôm anh lẹ đi chứ còn chờ gì nữa',
+    btnAccept: 'Không thấy tui đâu thì nhấn nút này gọi cho tui',
+    messLink: 'https://m.me/knebao' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
